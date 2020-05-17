a0fa9113ee5ebba5f83d69e99d6dee12b96cd9f4
Ripudaman Teja <rteja@asu.edu>
2020-05-16 22:04:10 -0700

    Started MessagesScreen

--- App.js
+++ App.js
@@ -4,9 +4,13 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
   return (
-    <ListingDetailsScreen />
+    // <ListingDetailsScreen />
+    // <ViewImageScreen />
+    <MessagesScreen />
   );
 }
