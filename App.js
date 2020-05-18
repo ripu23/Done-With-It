df04914bae7a7815854fc2fdd8ccecdca029d134
Ripudaman Teja <rteja@asu.edu>
2020-05-18 16:43:17 -0700

    LoginScreen finished. Refactored

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, TextInput } from 'react-native';
 
+import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -11,13 +11,43 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
+  const categories = [
+    {
+      label: 'Furniture',
+      value: 1
+    },
+    {
+      label: 'Clothing',
+      value: 2
+    },
+    {
+      label: 'Cameras',
+      value: 3
+    }
+  ];
+  const [category, setCategory] = useState();
+
   return (
     // <ListingDetailsScreen />
     // <ViewImageScreen />
     // <MessagesScreen />
     // <AccountScreen />
-    <ListingsScreen />
+    // <ListingsScreen />
+    // <Screen>
+    //   <AppPicker
+    //     selectedItem={category}
+    //     onSelectItem={item => setCategory(item)}
+    //     items={categories}
+    //     icon='apps'
+    //     placeholder='category'
+    //   />
+    //   <AppTextInput icon='email' placeholder='email' />
+    // </Screen>
+    <LoginScreen />
   );
 }
